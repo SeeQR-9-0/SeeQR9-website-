d70c592f047f04f4c8a6cdfba59da49fa9dee0ea
utyvert <utkarshuppal@gmail.com>
2023-02-07 12:37:00 -0800

    test

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>SeeQR</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="SeeQR - an open source database dev tool." />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.png" />
     </>
   )
 }

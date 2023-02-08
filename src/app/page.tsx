1875ebdb0b39d639e5c5bd42444f16f2355085a5
utyvert <utkarshuppal@gmail.com>
2023-02-08 10:44:07 -0800

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,47 @@
-'use client'
+"use client"
 
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
-import ReactNavbar from "react-responsive-animate-navbar";
 import MenuIcon from "@mui/icons-material/Menu";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <Link className={styles.brand} href="/">
+        My Website
+      </Link>
+      <MenuIcon className={styles.menuIcon} />
+    </nav>
+  );
+};
+
+const Item = ({ title }: { title: string }) => {
+  return (
+    <div className={styles.item}>
+      <h2>{title}</h2>
+    </div>
+  );
+};
+
+const Home = () => {
+  const [items, setItems] = useState<string[]>(['Home', 'Team', 'Github']);
+
+
   return (
     <main className={styles.main}>
+      <Navbar>
+      <div className={styles.container}>
+        {items.map((item) => (
+          <Item key={item} title={item} />
+        ))}
+      </div>
+      </Navbar>
     </main>
   );
-}
+};
+
+export default Home;

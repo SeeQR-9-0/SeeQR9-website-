9c9ad9819a61b419f13f43e47ca7368b654ac394
utyvert <utkarshuppal@gmail.com>
2023-02-23 11:07:49 -0800

    download links added

--- src/components/DownloadModule.tsx
+++ src/components/DownloadModule.tsx
@@ -12,18 +12,17 @@ const DownloadModule = () => {
     // TODO: Register url click here
     window.location.href = url;
   };
-
   return (
     <div className="download-module">
       <span className="download-text">Download:</span>
       <div className="dl-buttons">
-        <div className="download-button" onClick={() => handleDownload("")}>
+        <div className="download-button" onClick={() => handleDownload('https://github.com/open-source-labs/SeeQR/releases/download/v9.0.1/SeeQR-9.0.1.dmg')}>
           <img src="/media/dl_icons/mac.png" alt="Download for Mac" />
         </div>
-        <div className="download-button" onClick={() => handleDownload("")}>
+        <div className="download-button" onClick={() => handleDownload("https://github.com/open-source-labs/SeeQR/releases/download/v9.0.1/SeeQR-9.0.1-win.zip")}>
           <img src="/media/dl_icons/windows.png" alt="Download for Windows" />
         </div>
-        <div className="download-button" onClick={() => handleDownload("")}>
+        <div className="download-button" onClick={() => handleDownload("https://github.com/open-source-labs/SeeQR/releases/download/v9.0.1/SeeQR-9.0.1.AppImage")}>
           <img src="/media/dl_icons/linux.png" alt="Download for Linux" />
         </div>
       </div>
